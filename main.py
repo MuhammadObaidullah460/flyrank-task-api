b2f9b138f2eb7fd644ff7609154fc36a39a2ba91
Obaid Minhas <minhasobaid8@gmail.com>
2026-08-10 10:33:35 +0500

    Stage 0: hello server

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]}
